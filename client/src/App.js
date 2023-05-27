4193a32872eb7af3945ad25a73557bebf27041be
Dalejandro31 <74673610+Dalejandro31@users.noreply.github.com>
2023-05-26 21:13:38 -0500

    commitInit

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,10 @@
 import './App.css';
-
+import {Routes, Route} from 'react-router-dom'
 function App() {
   return (
-    <div className="App">
-      <h1>Henry Countries</h1>
-    </div>
+    <Routes>
+      <Route/>
+    </Routes>
   );
 }
 
