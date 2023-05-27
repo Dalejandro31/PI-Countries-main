8e2786996008bce191c16a9d7361acf9fd4235b6
Dalejandro31 <74673610+Dalejandro31@users.noreply.github.com>
2023-05-27 15:43:02 -0500

    commit05

--- client/src/components/HomePage/HomePage.jsx
+++ client/src/components/HomePage/HomePage.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { getAllCountries } from "../../redux/actions";
 import Card from '../Card/CardCountry';
@@ -6,16 +6,23 @@ import Card from '../Card/CardCountry';
 function Home(){
   const dispatch = useDispatch();
   const countries = useSelector(state => state.countries);
+  const itemsPerpage = 9;
+  const [currentPage, setCurrentPage] = useState(1);
 
   useEffect(()=>{
     dispatch(getAllCountries());
   },[dispatch]);
 
+  //logica para obtener los paises para la pagina actual 
+  const startIndex = (currentPage - 1) * itemsPerpage;
+  const endIndex = currentPage + itemsPerpage;
+  const currentCountries = countries.slice(startIndex, endIndex);
+
   return(
     <div>
       <h1>Home Page</h1>
       <div>
-        {countries.map((country) =>(
+        {currentCountries.map((country) =>(
           <Card
             key={country.id}
             id={country.id}
@@ -24,6 +31,14 @@ function Home(){
             region={country.region}
           />
         ))}
+      </div>
+      <div>
+        <button disabled={currentPage === 1} onClick={()=> setCurrentPage((prevPage) => prevPage -1)}>
+          Previous
+        </button>
+        <button disabled={endIndex >= countries.length} onClick={()=> setCurrentPage((prevPage) => prevPage + 1)}>
+          Next
+        </button>
       </div>
     </div>
     
